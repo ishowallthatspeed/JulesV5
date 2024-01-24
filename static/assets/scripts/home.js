61476d0b2c5c9fea40f1058d8abb88d26563b1ac
ishowallthatspeed <133022362+ishowallthatspeed@users.noreply.github.com>
2024-01-23 21:18:10 -0700

    Update home.js

--- static/assets/scripts/home.js
+++ static/assets/scripts/home.js
@@ -6,11 +6,7 @@ try {
   inFrame = true
 }
 
-if (!inFrame && !navigator.userAgent.includes('Firefox')) {
-  const popup = open('about:blank', '_blank')
-  if (!popup || popup.closed) alert('Please allow popups and redirects.')
-  else {
-    popup.document.write(`
+
             <iframe src="${
               location.href
             }" style="position:fixed;top:0;left:0;width:100%;height:100%;outline:none;border:none;"></iframe>
